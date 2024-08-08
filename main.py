2f2684cfa7e660f542a7a2e76ecceb023ba367c7
Daniel Kenan Slinda <sdanielkenan@gmail.com>
2024-08-08 15:35:24 +0200

    update remote handling video

--- main.py
+++ main.py
@@ -15,29 +15,29 @@ def call():
 
 @socketio.on('join')
 def on_join(data):
-    room = data.get('room', '1')  # Default to room 1 if no room is provided
+    room = data.get('room')
     join_room(room)
-    emit('status', {'msg': 'User has entered the room.'}, room=room)
+    emit('status', {'msg': f'User has entered the room {room}.'}, room=room)
 
 @socketio.on('leave')
 def on_leave(data):
-    room = data.get('room', '1')  # Default to room 1 if no room is provided
+    room = data.get('room')
     leave_room(room)
-    emit('status', {'msg': 'User has left the room.'}, room=room)
+    emit('status', {'msg': f'User has left the room {room}.'}, room=room)
 
 @socketio.on('offer')
 def handle_offer(data):
-    room = data.get('room', '1')  # Ensure the room is considered
+    room = data.get('room')
     emit('offer', data, room=room)
 
 @socketio.on('answer')
 def handle_answer(data):
-    room = data.get('room', '1')  # Ensure the room is considered
+    room = data.get('room')
     emit('answer', data, room=room)
 
 @socketio.on('ice-candidate')
 def handle_ice_candidate(data):
-    room = data.get('room', '1')  # Ensure the room is considered
+    room = data.get('room')
     emit('ice-candidate', data, room=room)
 
 if __name__ == '__main__':
